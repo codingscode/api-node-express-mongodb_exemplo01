1737444dc04c1d9f34e2ddd3d1486e4b2373536e
codingscode <heronnestg@hotmail.com>
2023-03-22 10:25:33 -0300

    parte 5

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,17 @@
 
 import express from 'express'
+import mongoose from 'mongoose'
 
 
 const app = express()
 
-app.use('/api', (req, res, next) => {
-   res.send('Olá mundo!')
-})
 
-app.listen(5000)
+
+mongoose.connect('mongodb+srv://admin:aladin18@cluster0.zgjyqfq.mongodb.net/Blog?retryWrites=true&w=majority')
+      .then(() => app.listen(5000))
+      .then( () => console.log('conectado ao banco de dados e listening o localhost 5000') )
+      .catch((err) => console.log(err))  
+
+
 
 
