57e5611268a77757484c22b84470ad4c0cbaf899
codingscode <heronnestg@hotmail.com>
2023-03-23 22:42:49 -0300

    parte 12

--- backend/controllers/blog-controller.js
+++ backend/controllers/blog-controller.js
@@ -31,6 +31,34 @@ export const addBlog = async (req, res, next) => {
    
 }
 
+export const updateBlog = async (req, res, next) => {
+   const { title, description } = req.body
+   const blogId = req.params.id
+   let blog
+   
+   try {
+      blog = await Blog.findByIdAndUpdate(blogId, {
+         title, description      
+      })
+   }
+   catch (err) {
+      return console.log(err)
+   }
+   
+   if (!blog) {
+      return res.status(500).json({message: 'Inapito para atualizar o Blog!'})
+   }
+   return res.status(200).json({blog})
+
+}
+
+
+
+
+
+
+
+
 
 
 
