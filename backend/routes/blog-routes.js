f062140eeed7da5a69d951b9a354626e6ee4bab5
codingscode <heronnestg@hotmail.com>
2023-03-23 22:09:21 -0300

    parte 11

--- backend/routes/blog-routes.js
+++ backend/routes/blog-routes.js
@@ -1,12 +1,13 @@
 
 import express from 'express'
-import { getAllBlogs } from '../controllers/blog-controller'
+import { addBlog, getAllBlogs } from '../controllers/blog-controller'
 
 
 const blogRouter = express.Router()
 
 
 blogRouter.get('/', getAllBlogs)
+blogRouter.post('/add', addBlog)
 
 
 export default blogRouter
