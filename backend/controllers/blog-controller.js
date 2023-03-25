4f73ccb567d5ede78fcabab48c651deb2a841baa
codingscode <heronnestg@hotmail.com>
2023-03-25 11:41:25 -0300

    parte 17

--- backend/controllers/blog-controller.js
+++ backend/controllers/blog-controller.js
@@ -1,5 +1,7 @@
 
 import Blog from '../model/Blog'
+import User from '../model/User'
+import mongoose from 'mongoose'
 
 
 export const getAllBlogs = async (req, res, next) => {
@@ -19,13 +21,33 @@ export const getAllBlogs = async (req, res, next) => {
 
 export const addBlog = async (req, res, next) => {
    const { title, description, image, user } = req.body
-   const blog = new Blog({ title, description, image, user })
 
+   let existingUser
+   
    try {
-      await blog.save()
+      existingUser = await User.findById(user)
    }
    catch (err) {
       return console.log(err)
+   }
+   
+   if (!existingUser) {
+      return res.status(400).json({ message: 'Inapito para encontrar usuário por este Id' })
+   }
+
+   const blog = new Blog({ title, description, image, user })
+
+   try {
+      const session = await mongoose.startSession()
+      session.startTransaction()
+      await blog.save({ session })
+      existingUser.blogs.push(blog)
+      await existingUser.save({ session })
+      await session.commitTransaction()
+   }
+   catch (err) {
+      console.log(err)
+      return res.status(500).json({ message: err })
    }
    return res.status(200).json({blog})
    
@@ -86,29 +108,6 @@ export const deleteBlog = async (req, res, next) => {
 
    return res.status(200).json({message: 'Deletado com sucesso!'})
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
