38ac9c1a6c92890590c468f16abb99dd7dfc2205
codingscode <heronnestg@hotmail.com>
2023-04-17 22:15:03 -0300

    parte 20

---
+++ README.md
@@ -0,0 +1,2 @@
+# Usando Node, Express e MongoDB
+Consumindo Rest Api Node Express e MongoDB
--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use('/api/user', router)  //http://localhost:5000/api/user/logon
 app.use('/api/blog', blogRouter)
 
 
-mongoose.connect('mongodb+srv://admin:aladin18@cluster0.zgjyqfq.mongodb.net/Blog?retryWrites=true&w=majority')
+mongoose.connect('mongodb://localhost:27017/backend1')  //  mongoose.connect('mongodb+srv://usuario:senha@cluster0.zgjyqfq.mongodb.net/Blog?retryWrites=true&w=majority')
       .then(() => app.listen(5000))
       .then( () => console.log('conectado ao banco de dados e listening o localhost 5000') )
       .catch((err) => console.log(err))  
